32a15392be95d118772984acd53ce400a96ed3b9
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-08 19:44:05 +0530

    changed home.html added counting template

--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Counter
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-def home(request):
-    return render(request, 'base/home.html')
+class HomeView(LoginRequiredMixin, View):
+    def get(self, request):
+        count = Counter.objects.get(user=request.user)
+        context = {
+            'count':count
+        }
+        return render(request, 'base/home.html', context)
