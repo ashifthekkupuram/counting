6d6666d73abc4748fbf211eaf02fcd260fc7010e
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-08 19:52:59 +0530

    added simple printing hi when clicking buttons in js

--- base/static/base/base.js
+++ base/static/base/base.js
@@ -1,1 +1,7 @@
-console.log('hello')
+var btns = document.getElementsByClassName('action-button')
+
+for(var i=0;i<btns.length;i++){
+    btns[i].addEventListener('click', function (){
+        console.log('Hi')
+    })
+}
