655748ddc518bf5ec383e22f39327bc6d78ea3fb
Ashif Thekkupuram <ashifhussain4548@gmail.com>
2023-10-08 18:03:37 +0530

    added apps and packages in settings and given boostrap5

--- counting/settings.py
+++ counting/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    'users.apps.UsersConfig',
+    'crispy_bootstrap5',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPlATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
